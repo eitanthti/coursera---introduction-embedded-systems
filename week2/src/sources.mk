a8765370758def9413cb4e6f3a32a969e7ebf236
Eitan <eitanthti.e.h@gmail.com>
2021-01-23 17:52:53 -0700

    adding the all files

---
+++ week2/src/sources.mk
@@ -0,0 +1,7 @@
+#******************************************************************************
+#Eitan Halfon
+#eitanthti.e.h@gmail.com
+#*****************************************************************************
+
+SOURCES = main.c memory.c 
+INCLUDES = -I ../include/msp432 -I ../include/common -I../include/CMSIS 
